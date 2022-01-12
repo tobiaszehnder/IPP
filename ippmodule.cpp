85896326949463228f99093eccf8a9f4a4219c48
Sämy Zehnder <saemy.zehnder@gorrion.ch>
2022-01-12 22:30:17 +0100

    Avoid compiler error on older versions of gcc
    Initialize the PyIpp_Type struct in the old-fashioned way to avoid
    "non-trivial designated initializers not supported" errors.

--- ippmodule.cpp
+++ ippmodule.cpp
@@ -241,13 +241,43 @@ static PyMethodDef ippMethods[] = {
 static PyTypeObject PyIpp_Type = {
     PyVarObject_HEAD_INIT(NULL, 0)
 
-    .tp_name = "ipp.Ipp",
-    .tp_basicsize = sizeof(PyIpp),
-    .tp_itemsize = 0,
-    .tp_dealloc = (destructor)ippDealloc,
-    .tp_flags = Py_TPFLAGS_DEFAULT,
-    .tp_methods = ippMethods,
-    .tp_new = ippNew,
+    "ipp.Ipp",                /* tp_name */
+    sizeof(PyIpp),            /* tp_basicsize */
+    0,                        /* tp_itemsize */
+    (destructor)ippDealloc,   /* tp_dealloc */
+    0,                        /* tp_vectorcall_offset */
+    0,                        /* tp_getattr */
+    0,                        /* tp_setattr */
+    0,                        /* tp_as_async */
+    0,                        /* tp_repr */
+    0,                        /* tp_as_number */
+    0,                        /* tp_as_sequence */
+    0,                        /* tp_as_mapping */
+    0,                        /* tp_hash */
+    0,                        /* tp_call */
+    0,                        /* tp_str */
+    0,                        /* tp_getattro */
+    0,                        /* tp_setattro */
+    0,                        /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT,       /* tp_flags */
+    0,                        /* tp_doc */
+    0,                        /* tp_traverse */
+    0,                        /* tp_clear */
+    0,                        /* tp_richcompare */
+    0,                        /* tp_weaklistoffset */
+    0,                        /* tp_iter */
+    0,                        /* tp_iternext */
+    ippMethods,               /* tp_methods */
+    0,                        /* tp_members */
+    0,                        /* tp_getset */
+    0,                        /* tp_base */
+    0,                        /* tp_dict */
+    0,                        /* tp_descr_get */
+    0,                        /* tp_descr_set */
+    0,                        /* tp_dictoffset */
+    0,                        /* tp_init */
+    0,                        /* tp_alloc */
+    ippNew,                   /* tp_new */
 };
 
 static struct PyModuleDef ippModule = {
