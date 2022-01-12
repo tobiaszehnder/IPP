2857c3bda5c0d50917b709243b059ef273a540e9
Sämy Zehnder <saemy.zehnder@gorrion.ch>
2022-01-12 17:25:42 +0100

    Support ipp debug builds
    One can now build the ipp module without optimizations by setting the
    IPP_DEBUG environment variable.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 from distutils.extension import Extension
 import numpy as np
+import os
+
+extra_compile_args = ['-std=c++17']
+if os.environ.get("IPP_DEBUG"):
+    extra_compile_args.append('-O0')
 
 ipp_extension = Extension('ipp',
                           ['ippmodule.cpp', 'ipp.cpp'],
                           include_dirs=[np.get_include()],
-                          extra_compile_args=['-std=c++17'])
+                          extra_compile_args=extra_compile_args)
 setup(name='ipp',
       version='1.0',
       description='This is the IPP package',
