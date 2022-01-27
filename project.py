fd67c970a3fb3aab88306d01b8b08ccffe945137
tobiaszehnder <zehnder@molgen.mpg.de>
2022-01-27 16:50:17 +0100

    incorporated all functions into project.py, updated README.

--- project.py
+++ project.py
@@ -260,6 +260,29 @@ def main():
     with open(outfile_unmapped, 'w') as f:
         f.write('\n'.join(unmapped_regions) + '\n')
 
+
+    def classify_conservation(df_projections, target_regions=pr.PyRanges(), thresh=.95, maxgap=500):
+        ### function for determining the conservation of sequence (DC/IC/NC) and function (+/-)  
+        # determine sequence conservation
+        sequence_conservation = df_projections.apply(lambda x: 'DC' if x['score_direct'] == 1 else 'IC' if x['score_multi'] >= thresh else 'NC', axis=1).values
+        # create PyRanges object of projections
+        df_multi = pd.DataFrame({'Chromosome': [x.chrom for x in df_projections['coords_multi']],
+                                 'Start': [x.loc for x in df_projections['coords_multi']]})
+        df_multi['End'] = df_multi['Start'] + 1
+        pr_multi = pr.PyRanges(df_multi)  
+        # determine functional conservation by checking for overlap with target regions
+        functional_conservation = [''] * df_multi.shape[0]
+        if target_regions is not None:
+            # expand target regions by maxgap
+            target_regions.Start -= maxgap
+            target_regions.End += maxgap
+            # save names of projected regions that overlap with a target region
+            names_FC = target_regions.overlap(pr_multi).Name.values
+            functional_conservation = ['+' if x in names_FC else '-' for x in df_projections.index]
+        # combine information of sequence and functional conservation
+        conservation = [s+f for s,f in zip(sequence_conservation,functional_conservation)]
+        return conservation
+        
     # classify projections according to conservation of sequence (DC/IC/NC) and function (+/-)
     log('Classifying projections')
     thresh = .95
