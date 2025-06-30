dfb9d876cc851941ced675207a773393c2e263e0
tobias-zehnder <127836983+tobias-zehnder@users.noreply.github.com>
2025-07-01 00:29:22 +0200

    Test/dummy data (#14)
    * fix: trigger semantic release for testing
    
    * test: change number of cores to 1
    
    * chore: refresh commit history after rebase

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,5 +1,39 @@
-def test_cpp_backend_initialization():
-    from src.ipp.project import ipp_cpp
+import os
+import shutil
+import subprocess
+import tempfile
 
-    ipp = ipp_cpp.Ipp()
-    assert ipp is not None
+
+def test_cpp_backend_project_script_runs():
+    # Create a temporary output directory
+    tmpdir = tempfile.mkdtemp()
+
+    try:
+        # Run the script using uv run
+        _ = subprocess.run(
+            [
+                "uv",
+                "run",
+                "python",
+                "src/ipp/project.py",
+                "-o",
+                tmpdir,
+                "-n",
+                "1",
+                "data/dummy_regions.bed",
+                "mm39",
+                "hg38",
+                "data/dummy.pwaln.bin",
+            ],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        # Check files were created in tmpdir
+        assert os.path.isdir(tmpdir)
+        assert len(os.listdir(tmpdir)) > 0
+
+    finally:
+        # Clean up the temporary directory
+        shutil.rmtree(tmpdir)
